55d8188530a0502be5d2e00e887f1b860d9e5e74
VinceNguyen <nguyenquangv95@gmail.com>
2022-04-10 15:20:50 +0700

    add-param-for-query-in-graph-ql

--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -3,7 +3,10 @@ const { books, authors } = require("../data/static.js");
 const resolvers = {
   Query: {
     books: () => books,
+    book: (parent, args) => books.find((book) => book.id === Number(args.id)),
     authors: () => authors,
+    author: (parent, args) =>
+      authors.find((author) => author.id === Number(args.id)),
   },
 };
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,13 +8,15 @@ const typeDefs = gql`
   }
   type Author {
     id: ID!
-    name:String
-    age:Int
+    name: String
+    age: Int
   }
   # ROOT TYPE
   type Query {
     books: [Book]
+    book(id: ID!): Book
     authors: [Author]
+    author(id: ID!): Author
   }
 `;
 
